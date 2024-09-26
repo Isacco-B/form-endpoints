9afc4c6abd37e2a981ce76b4f96131cd2a00c4e9
Isacco-B <git@isaccobertoli.com>
2024-09-26 16:04:12 +0200

    general update

---
+++ server/middleware/index.js
@@ -0,0 +1,5 @@
+import errorHandler from "./errorHandler.js";
+import formValidator from "./fromValidator.js";
+import loginLimiter from "./loginLimiter.js";
+
+export { errorHandler, loginLimiter, formValidator };
